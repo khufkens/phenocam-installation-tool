a28a6e4199d42f2688e058cfd5806fff58228891
Koen Hufkens <khufkens@X201.(none)>
2014-03-26 10:14:20 -0400

    adding scripts contained within the gzip install file to repository

---
+++ phenocam_install.sh
@@ -0,0 +1,295 @@
+#!/bin/sh
+
+#--------------------------------------------------------------------
+# This script installs all necessary configuration
+# files as required to upload images to the PhenoCam server
+# (phenocam.sr.unh.edu) on your NetCam SC/XL camera
+#
+# NOTES: this program can be used stand alone or called remotely
+# as is done in the remote_phenocam_install.sh script. The script
+# will pull all installation files from a server and adjust the
+# settings on the NetCam accordingly.
+#
+# Koen Hufkens (January 2014) koen.hufkens@gmail.com
+#--------------------------------------------------------------------
+
+# -------------- SETTINGS -------------------------------------------
+
+# get todays date
+TODAY=`date +"%Y_%m_%d_%H%M%S"`
+
+# set the new camera name / could be the same if the camera
+# has not moved, or did not get a new function
+NEW_CAMERA_NAME=$1
+
+# The time offset of your camera position relative to UTC time
+# as specified in + or - XYZ hours
+TIMEOFFSET=$2
+
+# set local time zone string if available, otherwise default to LOCAL
+if [ -n "$3" ]; then
+    LOCALTZ=$3
+else
+    LOCALTZ="LOCAL"
+fi
+
+# set crontab start and end times default
+# to 4 and 22 if not provided, set interval
+# to 30 min
+
+if [ -n "$4" ]; then
+    CRONSTART=$4
+else
+    CRONSTART="4"
+fi
+
+if [ -n "$5" ]; then
+    CRONEND=$5
+else
+    CRONEND="22"
+fi
+
+if [ -n "$6" ]; then
+    CRONINT=$6
+else
+    CRONINT="30"
+fi
+
+# upload / download server - location from which to grab and
+# and where to put config files
+HOST='klima.sr.unh.edu'
+USER='anonymous'
+PASSWD='anonymous'
+
+# make sure we are in the config directory
+# before proceeding
+cd /etc/config
+
+# -------------- BACKUP OLD CONFIG ----------------------------------
+
+echo ""
+echo "#--------------------------------------------------------------------"
+echo "#"
+echo "# Backing up settings for $NEW_CAMERA_NAME !"
+echo "#"
+echo "#--------------------------------------------------------------------"
+echo ""
+
+
+# In order to keep track of all the state changes of the camera
+# we will upload the current camera configuration to the server.
+
+# remove previous zip files
+if [ -f $NEW_CAMERA_NAME\_backup_settings\_$TODAY.tar.gz ]; then
+	rm $NEW_CAMERA_NAME\_backup_settings\_$TODAY.tar.gz
+fi
+
+# archive all settings
+tar -cf $NEW_CAMERA_NAME\_backup_settings\_$TODAY.tar *.conf *.scr
+
+# zip stuff
+gzip $NEW_CAMERA_NAME\_backup_settings\_$TODAY.tar
+
+ftp -n << EOF
+	open $HOST
+	user $USER $PASSWD
+	cd data/$1
+	binary
+	put $NEW_CAMERA_NAME\_backup_settings\_$TODAY.tar.gz
+	bye
+EOF
+
+# delete uploaded tar archive
+rm $NEW_CAMERA_NAME\_backup_settings\_$TODAY.tar.gz
+
+# -------------- DOWNLOAD CONFIG FILES ------------------------------
+
+# download the config files from a server
+# using the netcam's ftp function (headless)
+# no user input required
+
+# check if default_* files are there
+# if so do not download them again
+if [ ! -f default_ftp.scr ]; then
+
+echo ""
+echo "#--------------------------------------------------------------------"
+echo "#"
+echo "# Downloading settings from $HOST !"
+echo "#"
+echo "#--------------------------------------------------------------------"
+echo ""
+
+# remove previous install files
+if [ -f phenocam_default_install.tar* ]; then
+	rm phenocam_default_install.tar*
+fi
+
+wget http://$HOST/data/configs/phenocam_default_install.tar.gz
+
+gunzip phenocam_default_install.tar.gz
+tar -xvf phenocam_default_install.tar
+rm phenocam_default_install.tar
+
+fi
+
+# -------------- SET CAMERA NAME ------------------------------------
+
+# NOTE!! : The system doesn't do well when overwriting the original files
+# using sed / awk, always use a tmp file. This is the reason for
+# moving the files from the default_ uploaded ones to the final
+# correct file names. It makes tracing errors more easy as well.
+
+# grab camera info and make sure it is an IR camera
+MODEL=`info | grep "NetCamSC" | cut -d'=' -f2`
+MODELNAME=`echo $MODEL | sed 's/NetCam/NetCam /g'`
+
+if [ "$MODEL" = "NetCamSC" ] ; then
+	MODELNAME="NetCam SC IR"
+else
+	MODELNAME="NetCam XL"
+fi
+
+# set proper camera names in all config files
+# and upload scripts
+cat default_overlay0.conf	| sed "s/mycamera/$NEW_CAMERA_NAME/g" | sed "s/netcammodel/$MODELNAME/g" | sed "s/LOCAL/$LOCALTZ/g" > bak_overlay0.conf
+cat default_ftp.scr 		| sed "s/mycamera/$NEW_CAMERA_NAME/g" > ftp.scr
+cat default_IR_ftp.scr 		| sed "s/mycamera/$NEW_CAMERA_NAME/g" > IR_ftp.scr
+
+# rewrite everything into new files, just to be sure
+cat default_video.conf 	> video.conf
+cat default_video0.conf > video0.conf
+cat default_ntp.server 	> ntp.server
+cat default_sched0.conf > sched0.conf
+
+# remove all default files
+rm default_*
+
+echo ""
+echo "#--------------------------------------------------------------------"
+echo "#"
+echo "# Adjusting setting files !"
+echo "#"
+echo "#--------------------------------------------------------------------"
+echo ""
+
+# -------------- APPLY NEW CONFIGURATION ----------------------------
+
+# set time zone
+# dump setting to config file
+SIGN=`echo $TIMEOFFSET | cut -c'1'`
+
+if [ "$SIGN" = "+" ]; then
+	echo "UTC$TIMEOFFSET" | sed 's/+/-/g' > /etc/config/TZ
+else
+	echo "UTC$TIMEOFFSET" | sed 's/-/+/g' > /etc/config/TZ
+fi
+
+# export to current clock settings
+export TZ=`cat /etc/config/TZ`
+
+echo "# The camera clock is set to UTC$TIMEOFFSET"
+echo "# validate this setting - set date/time shown below !!!"
+date
+
+# convert the sign from the UTC time zone TZ variable (for plotting in overlay)
+if [ "$SIGN" = "+" ]; then
+	TZONE=`echo "$TZ" | sed 's/-/+/g'`
+else
+	TZONE=`echo "$TZ" | sed 's/+/-/g'`
+fi
+
+# adjust overlay settings to reflect current time baseline (UTC)
+cat bak_overlay0.conf  | sed "s/TZONE/$TZONE/g" > overlay0.conf
+
+# check if the exposure grid is altered from factory default
+# if so retain this exposure grid but alter all other settings
+# this avoids resetting exposure grids on certain funky cameras
+# which have been adjusted previously but might still benefit 
+# from an update (e.g. having the meta-data pushed etc).
+factory_grid="0xffffffffffffffff"
+current_grid=`cat /dev/video/config0 | grep 'exposure_grid' | cut -d'=' -f2`
+phenocam_grid=`cat video.conf | grep 'exposure_grid' | cut -d'=' -f2`
+
+if [ "$factory_grid" != "$current_grid" ];then
+	cat video.conf | sed -e s/"$phenocam_grid"/"$current_grid"/g > tmp.conf
+	
+	# overwrite the PhenoCam default settings with those
+	# preserving the old exposure grid
+	mv tmp.conf video.conf
+fi
+
+# dump video configuration to /dev/video/config0
+# device to adjust in memory settings
+nrfiles=`awk 'END {print NR}' video.conf`
+
+for i in `seq 1 $nrfiles` ;
+do
+ # assign a shell variable to a awk parameter with
+ # the -v statement
+ awk -v p=$i 'NR==p' video.conf > /dev/video/config0
+done
+
+# dump overlay configuration to /dev/video/config0
+# device to adjust in memory settings
+nrfiles=`awk 'END {print NR}' overlay0.conf`
+
+for i in `seq 1 $nrfiles` ;
+do
+ # assign a shell variable to a awk parameter with
+ # the -v statement
+ awk -v p=$i 'NR==p' overlay0.conf > /dev/video/config0
+done
+
+# cycle the clocks settings by calling the rc script
+# which governs NTP settings
+rc.ntpdate
+
+# -------------- SET SCHEDULED UPLOADS -------------------------------
+
+# set the cron job
+# this job calls the phenocam_upload.sh script and
+# upload a RGB and IR picture (if available) to the
+# phenocam servers
+
+echo ""
+echo "#--------------------------------------------------------------------"
+echo "#"
+echo "# Setting a crontab for timed uploads to the PhenoCam network !"
+echo "#"
+echo "#--------------------------------------------------------------------"
+echo ""
+
+# grap the default settings
+cat /etc/default/crontab > crontab
+
+# append the custom line
+echo "*/$CRONINT $CRONSTART-$CRONEND * * * admin sh /etc/config/phenocam_upload.sh" >> crontab
+
+# -------------- SAVE CONFIG / UPLOAD TEST IMATES -----------------------
+
+echo ""
+echo "#--------------------------------------------------------------------"
+echo "#"
+echo "# Saving config files to flash memory / uploading test images !"
+echo "#"
+echo "#--------------------------------------------------------------------"
+echo ""
+
+# !!!! MOST IMPORTANT, SAVE CONFIG TO FLASH !!!!
+config save
+
+# uploading first images, testing the upload procedure
+echo "Uploading the first images as a test... (wait 30s)"
+sh phenocam_upload.sh
+
+echo ""
+echo "#--------------------------------------------------------------------"
+echo "#"
+echo "# Done !!! - close the terminal if it remains open !"
+echo "#"
+echo "#--------------------------------------------------------------------"
+echo ""
+
+
+exit 0
---
+++ phenocam_upload.sh
@@ -0,0 +1,160 @@
+#!/bin/sh
+
+#--------------------------------------------------------------------
+# This script is cued up in the crontab file and called every
+# x min to upload two images, a standard RGB image and an infra-
+# red (IR) image (if available) to the PhenoCam server.
+#
+# last updated and maintained by:
+# Koen Hufkens (Januari 2014) koen.hufkens@gmail.com
+#--------------------------------------------------------------------
+
+# -------------- SETTINGS -------------------------------------------
+
+# make sure we are in the right directory
+cd /etc/config
+
+# export to current clock settings
+export TZ=`cat /etc/config/TZ`
+
+# switch time zone sign
+if [ -n `echo $TZ | grep -` ]; then
+	TZONE=`echo "$TZ" | sed 's/+/-/g'`
+else
+	TZONE=`echo "$TZ" | sed 's/-/+/g'`
+fi
+
+# config device (contains all settings and changing state variables)
+CONFIG="/dev/video/config0"
+
+# sets the delay between the
+# RGB and IR image acquisitions
+DELAY=30
+
+# sets debug state, for script development only
+# operational value is 0
+DEBUG=1
+if [ "$DEBUG" = "1" ] ; then
+LOG="/var/tmp/IR_upload.log"
+rm -f $LOG > /dev/null
+else
+LOG="/dev/null"
+fi
+
+# -------------- UPLOAD IMAGES --------------------------------------
+
+# grab camera info and make sure it is an IR camera
+MODEL=`info | grep "NetCamSC" | cut -d'=' -f2`
+
+# grab camera temperature from memory put it into
+# variable TEMP
+TEMP=`/bin/mbus -td2 /dev/ds1629 w 0xee w 0xaa,0 r 2 | awk '{ C = and(rshift($1, 7), 0x1ff); if (and(C, 0x100)) C = C - 0x200; C /= 2; printf("%.1f\n", C); }'`
+
+# grab date - keep fixed for RGB and IR uploads
+DATE=`date +"%a %b %d %Y %H:%M:%S"`
+
+# grap date and time string to be inserted into the
+# ftp scripts - this coordinates the time stamps
+# between the RGB and IR images (otherwise there is a
+# slight offset due to the time needed to adjust exposure
+DATETIMESTRING=`date +"%Y_%m_%d_%H%M%S"`
+
+# substitute the values in the ftp.scr and IR_ftp.scr
+# upload scripts
+cat ftp.scr | sed "s/DATETIMESTRING/$DATETIMESTRING/g" > ftp_tmp.scr
+cat IR_ftp.scr | sed "s/DATETIMESTRING/$DATETIMESTRING/g" > IR_ftp_tmp.scr
+
+# if it's a NetCamSC model make an additional IR picture
+# if not just take an RGB picture
+if [ "$MODEL" = "NetCamSC" ]; then
+
+	# just in case, set IR to 0
+	echo "ir_enable=0" > $CONFIG
+	sleep $DELAY # adjust exposure
+
+	# The following few lines updates the time in the overlay
+	# should time changes have occured it will show up in the
+	# overlay!!
+	# adjust overlay settings to reflect current time (UTC)
+	cat bak_overlay0.conf  | sed "s/TZONE/$TZONE/g" | sed "s/%a %b %d %Y  %H:%M:%S/$DATE/g" | sed "s/\${IC}/$TEMP/g" > overlay0_tmp.conf
+
+	# dump overlay configuration to /dev/video/config0
+	# device to adjust in memory settings
+	nrfiles=`awk 'END {print NR}' overlay0_tmp.conf`
+
+	for i in `seq 1 $nrfiles` ;
+	do
+	 awk -v p=$i 'NR==p' overlay0_tmp.conf > /dev/video/config0
+	done
+
+	# run the upload script With RGB enabled (default)
+	ftpscript ftp_tmp.scr >> $LOG
+
+	# change the settings to enable IR image acquisition
+	echo "ir_enable=1" > $CONFIG
+	sleep $DELAY	# adjust exposure
+
+	# dump overlay configuration to /dev/video/config0
+	# device to adjust in memory settings
+	nrfiles=`awk 'END {print NR}' overlay0_tmp.conf`
+
+	for i in `seq 1 $nrfiles` ;
+	do
+	 awk -v p=$i 'NR==p' overlay0_tmp.conf > /dev/video/config0
+	done
+
+	# run the upload script With IR enabled
+	ftpscript IR_ftp_tmp.scr >> $LOG
+
+	# Reset the configuration to 
+	# the default RGB settings
+	echo "ir_enable=0" > $CONFIG
+
+	# clean up temporary files
+	rm ftp_tmp.scr
+	rm IR_ftp_tmp.scr
+
+else
+
+	# The following few lines updates the time in the overlay
+	# should time changes have occured it will show up in the
+	# overlay!!
+	# adjust overlay settings to reflect current time (UTC)
+	cat bak_overlay0.conf  | sed "s/TZONE/$TZONE/g" | sed "s/%a %b %d %Y  %H:%M:%S/$DATE/g" | sed "s/\${IC}/$TEMP/g" > overlay0_tmp.conf
+
+	# just in case, set IR to 0
+	echo "ir_enable=0" > $CONFIG
+	sleep $DELAY # adjust exposure
+
+	# dump overlay configuration to /dev/video/config0
+	# device to adjust in memory settings
+	nrfiles=`awk 'END {print NR}' overlay0_tmp.conf`
+
+	for i in `seq 1 $nrfiles` ;
+	do
+	 awk -v p=$i 'NR==p' overlay0_tmp.conf > /dev/video/config0
+	done
+
+	# run the upload script With RGB enabled (default)
+	ftpscript ftp_tmp.scr >> $LOG
+
+	# clean up temporary files
+	rm ftp_tmp.scr
+fi
+
+# restore overlay with auto update for online viewing
+nrfiles=`awk 'END {print NR}' overlay0.conf`
+
+for i in `seq 1 $nrfiles` ;
+do
+ awk -v p=$i 'NR==p' overlay0.conf > /dev/video/config0
+done
+
+# clean up shared (between setup) temporary files
+rm overlay0_tmp.conf
+
+# Reset the configuration to 
+# the default RGB settings (just in case it's stuck at IR)
+echo "ir_enable=0" > $CONFIG
+
+exit
