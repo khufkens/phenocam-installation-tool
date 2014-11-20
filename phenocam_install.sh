bad56ef09a0804e06b8e3eae76ac8ffc20db9e74
Koen Hufkens <koen.hufkens@gmail.com>
2014-11-20 14:12:17 -0500

    Updated the download of install settings, now fetches from bitbucket instead of the PhenoCam server

--- phenocam_install.sh
+++ phenocam_install.sh
@@ -164,7 +164,9 @@ if [ -f phenocam_default_install.tar* ]; then
 	rm phenocam_default_install.tar*
 fi
 
-wget http://$HOST/data/configs/phenocam_default_install.tar.gz
+#wget http://$HOST/data/configs/phenocam_default_install.tar.gz
+# use the hardcoded bitbucket 'latest version'
+wget https://bitbucket.org/khufkens/phenocam-installation-tool/src/0e664438147ccde8537ca4703d0f62ecc43a2675/phenocam_default_install.tar.gz
 
 gunzip phenocam_default_install.tar.gz
 tar -xvf phenocam_default_install.tar
@@ -181,7 +183,7 @@ fi
 
 # grab camera info and make sure it is an IR camera
 MODEL=`status | grep Product | cut -d'/' -f3 | cut -d'-' -f1`
-IR=`status | grep IR | tail -c 2`
+IR=`status | grep IR | sed -e 's#.*IR:\(\)#\1#' | cut -d' ' -f1`
 
 if [ "$MODEL" = "NetCamSC" ]; then
 	if [ "$IR" = "1" ]; then
