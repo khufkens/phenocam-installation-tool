f3dacf30a8f4b271bb66feb41acc896288993d10
Koen Hufkens <koen.hufkens@gmail.com>
2015-03-09 14:10:36 -0400

    revert back to phenocam server, camera wget does not support https and bitbucket by default redirects to the https link

--- PIT.sh
+++ PIT.sh
@@ -28,7 +28,7 @@ echo "$3"
 sleep 1
 echo "cd /etc/config"
 sleep 1
-echo "wget http://bitbucket.org/khufkens/phenocam-installation-tool/raw/master/phenocam_default_install.tar.gz"
+echo "wget http://phenocam.sr.unh.edu/data/configs/phenocam_default_install.tar.gz"
 sleep 1
 echo "gunzip phenocam_default_install.tar.gz"
 sleep 1
